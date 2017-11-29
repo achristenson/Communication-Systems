e5bdf5baf8c5831b7eee0988564f90247107ff3d
Alex Christenson <11860900+alch5436@users.noreply.github.com>
2017-11-28 20:55:49 -0500

    Just changing some names

--- convertFromBitStream.m
+++ convertFromBitStream.m
@@ -3,13 +3,13 @@ function data = convertFromBitStream(bitStream, N)
     % assuming bitStream is cell array of length equal to the # of groups
     % of blocks
     
-    n = 8; % number of rows of a block 
-    m = 8; % number of columns of a block 
+    m = 8; % number of rows of a block 
+    n = 8; % number of columns of a block 
     data = cell([1 length(bitStream)]);
     
     for i=1:length(bitStream)
         data{i} = bi2de(bitStream{i});
-        data{i} = reshape(data{i}, [n m N]);   %convert back to 3D array, N different arrays of 8x8 blocks
+        data{i} = reshape(data{i}, [m n N]); %convert back to 3D array, N different arrays of 8x8 blocks
     end
     
 end
--- convertToBitStream.m
+++ convertToBitStream.m
@@ -1,10 +1,8 @@
 function stream = convertToBitStream(array_3d, N)
     % N is the number of blocks to be grouped together
     
-    n = size(array_3d, 1); % number of rows of a block (8)
-    m = size(array_3d, 2); % number of columns of a block (8)
+    [m n numBlocks] = size(array_3d); % Rows, Columns, Number of Blocks
     reshapeSize = n * m * N;
-    numBlocks = size(array_3d, 3); %number of nxm blocks
     assert(mod(numBlocks,N)==0);  %make sure we can make N groups of blocks
     
     j=1;
@@ -12,7 +10,6 @@ function stream = convertToBitStream(array_3d, N)
        stream{j} = reshape(array_3d(:,:,i:i+N-1), [reshapeSize 1]);
        stream{j} = de2bi(stream{j});
        j=j+1;
-        
     end
     
 end
